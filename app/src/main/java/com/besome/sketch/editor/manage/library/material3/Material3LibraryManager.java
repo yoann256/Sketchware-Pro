b02a2c79cce37013e4819264c78c8fdc442461f6
Edward-NG-KOC <ishak.ishak.vm@gmail.com>
2025-01-27 09:44:25 +0100

    refactor: cleanup `Material3LibraryManager`

--- app/src/main/java/com/besome/sketch/editor/manage/library/material3/Material3LibraryManager.java
+++ app/src/main/java/com/besome/sketch/editor/manage/library/material3/Material3LibraryManager.java
@@ -1,29 +1,49 @@
 package com.besome.sketch.editor.manage.library.material3;
 
 import android.os.Environment;
-
 import com.google.gson.Gson;
 
 import java.io.File;
-import java.io.FileWriter;
 
+import a.a.a.jC;
 import pro.sketchware.utility.FileUtil;
 
 public class Material3LibraryManager {
 
     private final String sc_id;
+    private final File configFile;
     private ConfigData configData;
+    private final boolean isAppCompatEnabled;
 
     public Material3LibraryManager(String sc_id) {
         this.sc_id = sc_id;
-        File configFile = getConfigPath();
+        isAppCompatEnabled = jC.c(sc_id).c().isEnabled();
+        this.configFile = getConfigPath();
+        this.configData = loadOrCreateConfigData();
+    }
 
+    private File getConfigPath() {
+        return new File(Environment.getExternalStorageDirectory(),
+                ".sketchware" + File.separator + "data" + File.separator + sc_id + File.separator + "material_3.json");
+    }
+
+    private ConfigData loadOrCreateConfigData() {
         if (configFile.exists()) {
-            configData = loadConfigData(configFile);
-        } else {
-            configData = new ConfigData(false, false);
-            saveConfigData(configFile, configData);
+            String jsonData = FileUtil.readFileIfExist(configFile.getAbsolutePath());
+            if (!jsonData.isEmpty()) {
+                try {
+                    return new Gson().fromJson(jsonData, ConfigData.class);
+                } catch (Exception ignored) {
+                }
+            }
         }
+        ConfigData defaultConfig = new ConfigData(false, false);
+        saveConfigData(defaultConfig);
+        return defaultConfig;
+    }
+
+    private void saveConfigData(ConfigData configData) {
+        FileUtil.writeFile(configFile.getAbsolutePath(), new Gson().toJson(configData));
     }
 
     public ConfigData getConfigData() {
@@ -32,48 +52,15 @@ public class Material3LibraryManager {
 
     public void setConfigData(ConfigData configData) {
         this.configData = configData;
-        saveConfigData(getConfigPath(), configData);
-    }
-
-    public File getConfigPath() {
-        return new File(Environment.getExternalStorageDirectory(),
-                ".sketchware" + File.separator + "data" + File.separator + sc_id + File.separator + "material_3.json");
-    }
-
-    public ConfigData loadConfigData(File file) {
-        String jsonData = FileUtil.readFileIfExist(file.getAbsolutePath());
-        if (!jsonData.isEmpty()) {
-            try {
-                return new Gson().fromJson(jsonData, ConfigData.class);
-            } catch (Exception ignored) {
-            }
-        }
-        return new ConfigData(false, false); // Return default config if loading fails
-    }
-
-    public void saveConfigData(File file, ConfigData configData) {
-        try (FileWriter writer = new FileWriter(file)) {
-            new Gson().toJson(configData, writer);
-        } catch (Exception ignored) {
-        }
+        saveConfigData(configData);
     }
 
     public boolean isMaterial3Enabled() {
-        File configFile = getConfigPath();
-        if (configFile.exists()) {
-            ConfigData configData = loadConfigData(configFile);
-            return configData.isMaterial3Enabled();
-        }
-        return false;
+        return isAppCompatEnabled && configData.isMaterial3Enabled();
     }
 
     public boolean isDynamicColorsEnabled() {
-        File configFile = getConfigPath();
-        if (configFile.exists()) {
-            ConfigData configData = loadConfigData(configFile);
-            return configData.isMaterial3Enabled() && configData.isDynamicColorsEnabled();
-        }
-        return false;
+        return isAppCompatEnabled && configData.isMaterial3Enabled() && configData.isDynamicColorsEnabled();
     }
 
     public record ConfigData(boolean isMaterial3Enabled, boolean isDynamicColorsEnabled) {
